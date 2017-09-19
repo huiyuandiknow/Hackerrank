54e46ec64a638fe751a9364171e585205dfda9da
Hui-yu Yang <huiyu.y@gmail.com>
2017-09-18 21:08:13 -0700

    Update Arrays.py

--- Data Structures/Arrays.py
+++ Data Structures/Arrays.py
@@ -7,10 +7,25 @@
 
 import sys
 
-
 n = int(input().strip())
 arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
 while n != 0: 
     print((str(arr[-1]) + ' '), end='')
     arr = arr[:-1]
     n -= 1
+	
+## "Sparse Arrays"
+## There is a collection of N strings ( There can be multiple occurences of a particular string ). 
+## Each string's length is no more than 20 characters. There are also Q queries. For each query, 
+## you are given a string, and you need to find out how many times this string occurs in the 
+## given collection of N strings.
+
+import sys
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+while n != 0: 
+    print((str(arr[-1]) + ' '), end='')
+    arr = arr[:-1]
+    n -= 1	
+	
